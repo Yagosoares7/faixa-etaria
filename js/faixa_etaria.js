269eee142bd39c1af227e7dd87e646d90d26d5f3
Yagosoares7 <yago.psoares@gmail.com>
2024-01-17 14:39:58 -0300

    Script para exibir a faixa etária do usuário
    Um script desenvolvido em JavaScript com a finalidade de exibir, na tela, a faixa etária do usuário baseado na idade informada pelo mesmo

---
+++ js/faixa_etaria.js
@@ -0,0 +1,13 @@
+var idade = prompt('Digite a sua idade: ');
+var idade = parseInt(idade);
+console.log(idade);
+
+if (idade >= 0 && idade < 15) {
+    document.write('Criança');
+} else if (idade >= 15 && idade < 30) {
+    document.write('Jovem');
+} else if (idade >= 30 && idade < 60) {
+    document.write('Adulto');
+} else {
+    document.write('Idoso');
+}
