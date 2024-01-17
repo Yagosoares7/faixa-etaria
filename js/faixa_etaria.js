667866946518270e53d24282973ce56ea7048c88
Yagosoares7 <yago.psoares@gmail.com>
2024-01-17 15:06:19 -0300

    Update faixa_etaria.js

--- js/faixa_etaria.js
+++ js/faixa_etaria.js
@@ -3,11 +3,11 @@ var idade = parseInt(idade);
 console.log(idade);
 
 if (idade >= 0 && idade < 15) {
-    document.write('Criança');
+    alert('Criança');
 } else if (idade >= 15 && idade < 30) {
-    document.write('Jovem');
+    alert('Jovem');
 } else if (idade >= 30 && idade < 60) {
-    document.write('Adulto');
+    alert('Adulto');
 } else {
-    document.write('Idoso');
+    alert('Idoso');
 }
